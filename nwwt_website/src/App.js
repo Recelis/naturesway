8f033d353ee1ee79205a365b0235ed89f6facebd
JackyRecelis <jackymlui@live.com>
2019-06-04 17:24:22 +1000

    setted up navbar

--- nwwt_website/src/App.js
+++ nwwt_website/src/App.js
@@ -1,49 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from "./pages/home.js";
+import NWWTNavbar from "./NWWTnavbar.js";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/healthandnutrition/">Health and Nutrition</Link>
-            </li>
-            <li>
-              <Link to="/units/">Units</Link>
-            </li>
-            <li>
-              <Link to="/collaboration/">Collaboration</Link>
-            </li>
-            <li>
-              <Link to="/ourteam/">Our Team</Link>
-            </li>
-            <li>
-              <Link to="/contact/">Contact</Link>
-            </li>
-          </ul>
+        <nav>
+          <NWWTNavbar />
         </nav>
-
         <Route path="/" exact component={Home} />
         <Route path="/healthandnutrition/" component={Healthandnutrition} />
         <Route path="/units/" component={Units} />
         <Route path="/collaboration/" component={Collaboration} />
         <Route path="/ourteam/" component={Ourteam} />
         <Route path="/contact/" component={Contact} />
-        </Router>
+      </Router>
     </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
 function Healthandnutrition() {
   return <h2>healthandnutrition</h2>;
---
+++ nwwt_website/src/NWWTnavbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from "react-router-dom";
+
+const NWWTNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand><Link to="/">Nature's Way Water Technologies</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link><Link to="/healthandnutrition/">Health and Nutrition</Link></Nav.Link>
+          <Nav.Link><Link to="/">Home</Link></Nav.Link>
+          <Nav.Link><Link to="/units/">Units</Link></Nav.Link>
+          <Nav.Link><Link to="/collaboration/">Collaboration</Link></Nav.Link>
+          <Nav.Link><Link to="/ourteam/">Our Team</Link></Nav.Link>
+          <Nav.Link><Link to="/contact/">Contact</Link></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NWWTNavbar;
